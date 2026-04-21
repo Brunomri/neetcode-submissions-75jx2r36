669237f231f2bd862cdcf72edb4125e5694edf8b
Bruno <19270033+Brunomri@users.noreply.github.com>
2026-04-21 19:47:06 -0300

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    public record Bar(int x, int y) {};
+
+    public int maxArea(int[] heights) {
+        int maxArea = 0;
+
+        /*for(int i = 0; i < heights.length; i++) {
+            for(int j = i; j < heights.length; j++) {
+                Bar leftBar = new Bar(i, heights[i]);
+                Bar rightBar = new Bar(j, heights[j]);
+                int area = getArea(leftBar, rightBar);
+                maxArea = area > maxArea ? area : maxArea;
+            }
+        }*/
+
+        int left = 0, right = heights.length - 1;
+
+        while(left < right) {
+            Bar leftBar = new Bar(left, heights[left]);
+            Bar rightBar = new Bar(right, heights[right]);
+            int area = getArea(leftBar, rightBar);
+            maxArea = area > maxArea ? area : maxArea;
+            
+            if(heights[left] < heights[right]) left++;
+            else right--;
+        }
+
+        return maxArea;
+    }
+
+    private int getArea(Bar leftBar, Bar rightBar) {
+        int deltaX = rightBar.x() - leftBar.x();
+        int y = Math.min(leftBar.y(), rightBar.y());
+        return deltaX * y;
+    }
+}
