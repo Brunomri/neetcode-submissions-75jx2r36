6c39f9d690a486b8261bb06f1906ed7e1e7123f3
Bruno <19270033+Brunomri@users.noreply.github.com>
2026-04-23 00:27:30 -0300

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public static final List<Character> operations = List.of('+', '-', '*', '/');
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> operands = new Stack<>();
+
+        if(tokens.length == 1) return Integer.valueOf(tokens[0]);
+
+        for(int i = 0; i < tokens.length; i++) {
+            if(tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")) {
+                int op2 = operands.pop();
+                int op1 = operands.pop();
+                operands.push(calculate(op1, op2, tokens[i]));
+            } else {
+                operands.push(Integer.valueOf(tokens[i]));
+            }
+        }
+        return operands.pop();
+    }
+
+    private int calculate(int op1, int op2, String operation) {
+        switch(operation) {
+            case "+":
+                System.out.println("%s + %s = %s".formatted(op1, op2, op1 + op2));
+                return op1 + op2;
+            case "-":
+                System.out.println("%s - %s = %s".formatted(op1, op2, op1 - op2));
+                return op1 - op2;
+            case "*":
+                System.out.println("%s * %s = %s".formatted(op1, op2, op1 * op2));
+                return op1 * op2;
+            case "/":
+                if (op2 == 0) return 0;
+                System.out.println("%s / %s = %s".formatted(op1, op2, op1 / op2));
+                return op1 / op2;
+        }
+        return 0;
+    }
+}
