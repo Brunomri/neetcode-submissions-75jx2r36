fa6e485ea29a1259952984d7db45d05571f8d656
Bruno <19270033+Brunomri@users.noreply.github.com>
2026-04-25 23:36:15 -0300

    Add: binary-search - submission-7

---
+++ Data Structures & Algorithms/binary-search/submission-7.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return bs(0, nums.length - 1, nums, target);
+    }
+
+    private int bs(int start, int end, int[] nums, int target) {
+        if(start > end) return -1;
+
+        int mid = start + (end - start) / 2;
+        
+        if(nums[mid] == target) return mid;
+        if(nums[mid] < target) return bs(mid + 1, end, nums, target);
+        return bs(start, mid - 1, nums, target);
+    }
+}
